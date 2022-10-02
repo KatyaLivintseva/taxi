fdc1ce449b1f49e6d863cabdf2d434146e62f5db
KatyaLivintseva <113354368+KatyaLivintseva@users.noreply.github.com>
2022-10-02 13:53:30 +0500

    Update taxi.py

--- taxi.py
+++ taxi.py
@@ -1,16 +1,16 @@
-print ('кол-во сотрудников (до 1000)')
+print ('Количество сотрудников (до 1000)')
 N=int(input())
 
-print ('расстояние до дома')
+print ('Расстояние до дома')
 rasstoyanie_dom = [{"сотрудник": i+1, "расстояние до дома": int(input())} for i in range(N)]
 
-print ('тариф такси за 1 км')
+print ('Тариф такси за 1 км')
 tarif=[int(input()) for i in range(N)]
 
 tarif.sort()
 rasstoyanie_dom.sort(key=lambda x: x["расстояние до дома"], reverse=True)
 print('')
 for i in range(N):
-    print('Сотрудник номер', rasstoyanie_dom[i]['сотрудник'], 'сядет в машину с тарифом', tarif[i])
-    print('Стоимость поездки составит', rasstoyanie_dom[i]['расстояние до дома']*tarif[i])
+    print('Сотрудник номер', rasstoyanie_dom[i]['сотрудник'], 'сядет в машину с тарифом', tarif[i], 'рублей')
+    print('Стоимость поездки составит', rasstoyanie_dom[i]['расстояние до дома']*tarif[i], 'рублей')
     print('')

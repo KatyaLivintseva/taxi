4aefb893da14dd57968de9579205885fa463d62e
KatyaLivintseva <113354368+KatyaLivintseva@users.noreply.github.com>
2022-10-03 15:28:31 +0500

    Update chislo.py

--- chislo.py
+++ chislo.py
@@ -7,118 +7,116 @@ def ch(chislo):
     E=['тысяча', 'тысячи', 'тысяч']
     R=['рубль', 'рубля', 'рублей']
 
-    stots=D[(tarif//100000)-1]
-    if ((tarif//100000)==0):
+    stots=D[(chislo//100000)-1]
+    if ((chislo//100000)==0):
         stots=''
         
-    dts=C[(tarif%100000//10000)-1]
-    if ((tarif%100000//10000)==0):
+    dts=C[(chislo%100000//10000)-1]
+    if ((chislo%100000//10000)==0):
         dts=''
         
-    ts=B[(tarif%10000//1000)-1]
-    if ((tarif%10000//1000)==0):
+    ts=B[(chislo%10000//1000)-1]
+    if ((chislo%10000//1000)==0):
         ts=''
         
-    sto=D[(tarif%1000//100)-1]
-    if ((tarif%1000//100)==0):
+    sto=D[(chislo%1000//100)-1]
+    if ((chislo%1000//100)==0):
         sto=''
         
-    ds=C[(tarif%100//10)-1]
-    if ((tarif%100//10)==0):
+    ds=C[(chislo%100//10)-1]
+    if ((chislo%100//10)==0):
         ds=''
         
-    odin=A[(tarif%10)-1]
-    if ((tarif%10)==0):
+    odin=A[(chislo%10)-1]
+    if ((chislo%10)==0):
         odin=''
 
-    if ((tarif%10000//1000)==0): 
+    if ((chislo%10000//1000)==0): 
         tsa=E[2]
-    if ((tarif%10000//1000)==1): 
+    if ((chislo%10000//1000)==1): 
         tsa=E[0]
-    if (1<(tarif%10000//1000)<5): 
+    if (1<(chislo%10000//1000)<5): 
         tsa=E[1]
-    if (4<(tarif%10000//1000)<10): 
+    if (4<(chislo%10000//1000)<10): 
         tsa=E[2]
-    if (10<(tarif//1000)<20):
+    if (10<(chislo//1000)<20):
         tsa=E[2]
-    if (10<(tarif//1000)<20):
+    if (10<(chislo//1000)<20):
         tsa=E[0]
-    if (10<(tarif//1000%100)<20):
+    if (10<(chislo//1000%100)<20):
         tsaa=E[2]
         
-    if ((tarif%10)==0): 
+    if ((chislo%10)==0): 
         rub=R[2]  
-    if ((tarif%10)==1): 
+    if ((chislo%10)==1): 
         rub=R[0]
-    if (1<(tarif%10)<5): 
+    if (1<(chislo%10)<5): 
         rub=R[1]
-    if (4<(tarif%10)<10):                
+    if (4<(chislo%10)<10):                
         rub=R[2]
 
-    if (tarif==1):
+    if (chislo==1):
         result = A[0]+' '+R[0]
-    if (tarif==10):
+    if (chislo==10):
         result = C[0]+' '+R[2]
-    if (1<tarif<5):
-        result = A[tarif-1]+' '+R[1]
-    if (4<tarif<10):
-        result = A[tarif-1]+' '+R[2]
-    if (10<tarif<20):
-        result = O[(tarif%10)-1]+' '+R[2]
-    if (10<(tarif%100)<20):
+    if (1<chislo<5):
+        result = A[chislo-1]+' '+R[1]
+    if (4<chislo<10):
+        result = A[chislo-1]+' '+R[2]
+    if (10<chislo<20):
+        result = O[(chislo%10)-1]+' '+R[2]
+    if (10<(chislo%100)<20):
         rub=R[2]
 
-    if (99999<tarif<1000000): #100 000 -> 999 999
-        if ((10<(tarif%100)<20) and (10<(tarif//1000%100)<20)):
-            result = stots+' '+O[(tarif//1000%100%10)-1]+' '+tsaa+' '+sto+' '+O[(tarif%10)-1]+' '+rub
+    if (99999<chislo<1000000): #100 000 -> 999 999
+        if ((10<(chislo%100)<20) and (10<(chislo//1000%100)<20)):
+            result = stots+' '+O[(chislo//1000%100%10)-1]+' '+tsaa+' '+sto+' '+O[(chislo%10)-1]+' '+rub
         else:
-            if (10<(tarif%100)<20):
-                result = stots+' '+dts+' '+ts+' '+tsa+' '+sto+' '+O[(tarif%10)-1]+' '+rub
+            if (10<(chislo%100)<20):
+                result = stots+' '+dts+' '+ts+' '+tsa+' '+sto+' '+O[(chislo%10)-1]+' '+rub
             else:
-                if (10<(tarif//1000%100)<20): 
-                    result = stots+' '+O[(tarif//1000%100%10)-1]+' '+tsaa+' '+sto+' '+ds+' '+odin+' '+rub
+                if (10<(chislo//1000%100)<20): 
+                    result = stots+' '+O[(chislo//1000%100%10)-1]+' '+tsaa+' '+sto+' '+ds+' '+odin+' '+rub
                 
                 else:
-                    if ((tarif//100000)==0 or (tarif%100000//10000)==0 or(tarif%10000//1000)==0 or (tarif%1000//100)==0 or (tarif%100//10)==0 or (tarif%10)==0):
+                    if ((chislo//100000)==0 or (chislo%100000//10000)==0 or(chislo%10000//1000)==0 or (chislo%1000//100)==0 or (chislo%100//10)==0 or (chislo%10)==0):
                         result = stots+' '+dts+' '+ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
                     else:
                         result = stots+' '+dts+' '+ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
                 
-    if (9999<tarif<100000): #10 000 -> 99 999
-        if ((10<(tarif//1000)<20) and (10<(tarif%100)<20)):
-            result = O[(tarif//1000%10)-1]+' '+tsaa+' '+sto+' '+O[(tarif%100%10)-1]+' '+rub
+    if (9999<chislo<100000): #10 000 -> 99 999
+        if ((10<(chislo//1000)<20) and (10<(chislo%100)<20)):
+            result = O[(chislo//1000%10)-1]+' '+tsaa+' '+sto+' '+O[(chislo%100%10)-1]+' '+rub
         else:
-            if (10<(tarif//1000)<20):
-                result = O[(tarif//1000%10)-1]+' '+tsaa+' '+sto+' '+ds+' '+odin+' '+rub
+            if (10<(chislo//1000)<20):
+                result = O[(chislo//1000%10)-1]+' '+tsaa+' '+sto+' '+ds+' '+odin+' '+rub
             else:
-                if (10<(tarif%100)<20):
-                    result = dts+' '+ts+' '+tsa+' '+sto+' '+O[(tarif%100%10)-1]+' '+rub
+                if (10<(chislo%100)<20):
+                    result = dts+' '+ts+' '+tsa+' '+sto+' '+O[(chislo%100%10)-1]+' '+rub
                 else:
-                    if ((tarif%100000//10000)==0 or(tarif%10000//1000)==0 or (tarif%1000//100)==0 or (tarif%100//10)==0 or (tarif%10)==0):
+                    if ((chislo%100000//10000)==0 or(chislo%10000//1000)==0 or (chislo%1000//100)==0 or (chislo%100//10)==0 or (chislo%10)==0):
                         result = dts+' '+ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
                     else:
                         result = dts+' '+ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
 
-    if (999<tarif<10000): #1000 -> 9999
-        if (10<(tarif%100)<20):
-            result = ts+' '+tsa+' '+sto+' '+O[(tarif%10)-1]+' '+rub
+    if (999<chislo<10000): #1000 -> 9999
+        if (10<(chislo%100)<20):
+            result = ts+' '+tsa+' '+sto+' '+O[(chislo%10)-1]+' '+rub
         else:
-            if ((tarif%10000//1000)==0 or (tarif%1000//100)==0 or (tarif%100//10)==0 or (tarif%10)==0):
+            if ((chislo%10000//1000)==0 or (chislo%1000//100)==0 or (chislo%100//10)==0 or (chislo%10)==0):
                 result = ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
             else:
                 result = ts+' '+tsa+' '+sto+' '+ds+' '+odin+' '+rub
 
-    if (99<tarif<1000): #100 -> 999
-        if (10<(tarif%100)<20):
-            result = sto+' '+O[(tarif%10)-1]+' '+rub
+    if (99<chislo<1000): #100 -> 999
+        if (10<(chislo%100)<20):
+            result = sto+' '+O[(chislo%10)-1]+' '+rub
         else:
-            if ((tarif%1000//100)==0 or (tarif%100//10)==0 or (tarif%10)==0):
+            if ((chislo%1000//100)==0 or (chislo%100//10)==0 or (chislo%10)==0):
                 result = sto+' '+ds+' '+odin+' '+rub
             else:
                 result = sto+' '+ds+' '+odin+' '+rub
 
-    if (19<tarif<100): #20 -> 99
+    if (19<chislo<100): #20 -> 99
         result = ds+' '+odin+' '+rub
-    return tarif
-
-
+    return result

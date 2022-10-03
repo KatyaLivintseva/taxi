78ee09d34d3e2df6f49e373edbeaeaa018591153
KatyaLivintseva <113354368+KatyaLivintseva@users.noreply.github.com>
2022-10-03 15:09:10 +0500

    Update taxi.py

--- taxi.py
+++ taxi.py
@@ -1,16 +1,26 @@
-print ('Количество сотрудников (до 1000)')
-N=int(input())
+from chislo import *
+N = int(input('Количество сотрудников (до 1000) '))
 
-print ('Расстояние до дома')
-rasstoyanie_dom = [{"сотрудник": i+1, "расстояние до дома": int(input())} for i in range(N)]
-
-print ('Тариф такси за 1 км')
-tarif=[int(input()) for i in range(N)]
-
-tarif.sort()
-rasstoyanie_dom.sort(key=lambda x: x["расстояние до дома"], reverse=True)
-print('')
+rasstoyanie_dom = []
 for i in range(N):
-    print('Сотрудник номер', rasstoyanie_dom[i]['сотрудник'], 'сядет в машину с тарифом', tarif[i], 'рублей')
-    print('Стоимость поездки составит', rasstoyanie_dom[i]['расстояние до дома']*tarif[i], 'рублей')
-    print('')
+    rasstoyanie_dom.append(int(input('Введите количество км до дома сотрудника ' + str(i + 1) + ': ')))
+
+tarif = []
+for i in range(N):
+    tarif.append(int(input(str(i + 1)+" такси с тарифом за 1 км" + ': ')))
+
+taxis = []
+sum = 0
+for i in range(N):
+    taxis.append(0)
+for i in range(N):
+    minR = rasstoyanie_dom.index(min(rasstoyanie_dom))
+    maxT = tarif.index(max(tarif))
+    taxis[minR] = maxT + 1
+    sum += min(rasstoyanie_dom) * max(tarif)
+    rasstoyanie_dom[minR] = max(rasstoyanie_dom) + 1
+    tarif[maxT] = 0
+
+for i in range(N):
+    print('Сотрудник',str(i + 1),'сядет в такси под номером', str(taxis[i]))
+print('\nМинимальная требуемая сумма с учетом введенных данных:', (sum))
